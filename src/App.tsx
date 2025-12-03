18617df3c4837d04f67b87c00077906139241831
THARUNJP <tharunroshini71@gmail.com>
2025-12-03 21:15:22 +0530

    c

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <div className="min-h-screen font-sans text-gray-900">
-        <main className="p-6">
-          <Routes>
-            <Route path="/" element={<div>Home Page</div>} />
-          </Routes>
-        </main>
-      </div>
+      <Routes>
+        <Route
+          path="/"
+          element={<div className="text-red-500">Home Page</div>}
+        />
+      </Routes>
     </BrowserRouter>
   );
 };
